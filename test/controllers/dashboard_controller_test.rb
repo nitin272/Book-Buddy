0a62cb66d69df524210d4782849b41dfd816b3ac
My-Boy001 <nitin.soni@kalvium.community>
2025-05-28 16:51:26 +0900

    test/dashboard updated

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:one)
-    post login_path, params: { email: @user.email, password: "password" }
+    post login_path, params: { session: { email: @user.email, password: "Nitin@11" } }
   end
   test "should get index" do
     get dashboard_index_url

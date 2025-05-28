8f933592211dd9195f6ca0b10753378731c67c29
My-Boy001 <nitin.soni@kalvium.community>
2025-05-28 16:34:29 +0900

    errors resolved

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,17 +2,17 @@ require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get sessions_new_url
+    get login_path
     assert_response :success
   end
 
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+  test "should create" do
+    post login_path, params: { email: "user@example.com", password: "password" }
+    assert_response :redirect
   end
 
   test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+    get logout_path
+    assert_response :redirect
   end
 end

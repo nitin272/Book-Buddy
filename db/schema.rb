dd37827d483868c75bad43ceac345a726e937d09
My-Boy001 <nitin.soni@kalvium.community>
2025-05-29 13:50:20 +0900

    Save local changes before merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_28_060704) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_29_043859) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "genre"
+    t.text "notes"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,4 +29,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_28_060704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "books", "users"
 end

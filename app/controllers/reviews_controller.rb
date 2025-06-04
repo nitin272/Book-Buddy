b6ba99225e82f5a9ab2a627b97724dae2da2a93f
My-Boy001 <nitin.soni@kalvium.community>
2025-06-04 13:23:24 +0900

    login path corrected

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,56 +1,59 @@
-class BooksController < ApplicationController
-  before_action :set_book, only: [ :show, :edit, :update, :destroy ]
-
+class ReviewsController < ApplicationController
+  # before_action :authenticate_user!
+  before_action :set_book
+  before_action :set_review, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    @books = current_user.books
+    @reviews = @book.reviews
   end
-
 
   def show
-    @review = @book.reviews.new
-  end
-
+  @book = Book.find(params[:book_id])
+  @review = @book.reviews.find(params[:id])
+end
 
   def new
-    @book = current_user.books.new
-  end
-
-
-  def create
-    @book = current_user.books.new(book_params)
-    if @book.save
-      redirect_to dashboard_path, notice: "Book was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
+    @review = @book.reviews.build
   end
 
   def edit
   end
 
-def update
-  if @book.update(book_params)
-    redirect_to dashboard_path(@book), notice: "Book was successfully updated."
+  def create
+  @review = @book.reviews.build(review_params)
+
+
+  if @review.save
+    redirect_to book_review_path(@book, @review), notice: "Review was successfully created."
   else
-    render :edit, status: :unprocessable_entity
+    render :new
   end
 end
 
-  def destroy
-  @book.destroy
-  redirect_to dashboard_path, notice: "Book was successfully deleted."
+def update
+  if @review.update(review_params)
+    redirect_to book_review_path(@book, @review), notice: "Review was successfully updated."
+  else
+    render :edit
+  end
 end
 
+def destroy
+  @review.destroy
+  redirect_to book_reviews_path(@book), notice: "Review was successfully destroyed."
+end
 
   private
 
   def set_book
-
-    @book = current_user.books.find(params[:id])
+    @book = Book.find(params[:book_id])
   end
 
-  def book_params
-    params.require(:book).permit(:title, :author, :genre, :notes)
+  def set_review
+    @review = @book.reviews.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
   end
 end
